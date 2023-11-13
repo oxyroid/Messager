7f8690865b1ece1cada0ac08ffd8950b58499abd
oxy <sortby@qq.com>
2023-11-13 23:52:56 +0800

    feat:jvm: modularity that dynamically adjusts based on window size.

--- composeApp/src/desktopMain/kotlin/ui/scaffolds/MailScaffold.desktop.kt
+++ composeApp/src/desktopMain/kotlin/ui/scaffolds/MailScaffold.desktop.kt
@@ -8,11 +8,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalWindowInfo
 import ui.LocalSpacing
 import ui.LocalTheme
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 actual fun MailScaffold(
     list: @Composable () -> Unit,
@@ -21,25 +24,31 @@ actual fun MailScaffold(
 ) {
     val theme = LocalTheme.current
     Row {
+        val windowInfo = LocalWindowInfo.current
+        val modularity = with(windowInfo.containerSize) { width > height }
+
         Box(Modifier.fillMaxHeight().weight(3f)) { list() }
-        Box(Modifier.fillMaxHeight().weight(5f)) {
-            val showDetail = detail != null
-            val currentBackgroundColor by animateColorAsState(
-                targetValue = if (showDetail) theme.primary
-                else Color.Gray
-            )
-            val currentContentColor by animateColorAsState(
-                targetValue = if (showDetail) theme.onPrimary
-                else Color.Black
-            )
-            MailScaffoldDetailBackground(
-                backgroundColor = currentBackgroundColor,
-                contentColor = currentContentColor
-            )
-            if (showDetail) {
-                detail?.invoke()
+        if (modularity) {
+            Box(Modifier.fillMaxHeight().weight(5f)) {
+                val showDetail = detail != null
+                val currentBackgroundColor by animateColorAsState(
+                    targetValue = if (showDetail) theme.primary
+                    else Color.Gray
+                )
+                val currentContentColor by animateColorAsState(
+                    targetValue = if (showDetail) theme.onPrimary
+                    else Color.Black
+                )
+                MailScaffoldDetailBackground(
+                    backgroundColor = currentBackgroundColor,
+                    contentColor = currentContentColor
+                )
+                if (showDetail) {
+                    detail?.invoke()
+                }
             }
         }
+
     }
 }
 
