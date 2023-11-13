66b115d5f6861f7e272e48f53c6eb50a83e557b9
oxy <sortby@qq.com>
2023-11-13 23:50:15 +0800

    build:jvm: set minimum window size.

--- composeApp/src/desktopMain/kotlin/main.kt
+++ composeApp/src/desktopMain/kotlin/main.kt
@@ -1,11 +1,16 @@
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
+import java.awt.Dimension
 
 fun main() = application {
     Window(
         title = "Messager",
-        onCloseRequest = ::exitApplication
+        onCloseRequest = ::exitApplication,
     ) {
+        LaunchedEffect(Unit) {
+            window.minimumSize = Dimension(360, 420)
+        }
         App()
     }
 }
